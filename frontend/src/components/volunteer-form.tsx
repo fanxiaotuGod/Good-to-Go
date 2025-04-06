e5b6c1c522edbc1c8b72e803bf8d96ac7415b80c
Nimesh Pandey <nimeshpnd14@gmail.com>
2025-04-05 20:27:39 -0700

    volunteer_form_resolved

--- frontend/src/components/volunteer-form.tsx
+++ frontend/src/components/volunteer-form.tsx
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 "use client"
 
 import { useState } from "react"
 import { z } from "zod"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-=======
-/* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any, react/no-unescaped-entities */
->>>>>>> 1531568 (api-addition)
 
 import { Button } from "@/components/ui/button"
 import {
